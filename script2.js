605cbbd62bb7b7bd53621d54a2ca4a53fbd9187a
Rescord <alexeypermiakov.ws@gmail.com>
2023-03-22 19:42:38 +0500

    add event for newBtn

---
+++ script2.js
@@ -0,0 +1,28 @@
+const btnAdd = document.querySelector(".buttons__create");
+const btnDelete = document.querySelector(".buttons__delete");
+const list = document.querySelector(".list__content");
+let a = 0;
+
+btnAdd.onclick = function () {
+  let dataOfUser = prompt("Введите то, что захотите");
+  newLi = document.createElement("li");
+  newLi.innerHTML = dataOfUser;
+  newLi.className = "Number" + a++;
+  newBtn = document.createElement("button");
+  newBtn.innerHTML = "Delete";
+  newBtn.className = newLi.className;
+  list.append(newLi);
+  newLi.append(newBtn);
+  newBtn.onclick = function (e) {
+    let target = e.target;
+    target.remove();
+  };
+};
+
+btnDelete.onclick = function () {
+  if (list.hasChildNodes()) {
+    list.removeChild(list.lastChild);
+  } else {
+    alert("Нечего удалять");
+  }
+};
